a27b2f16dcd8d764cac56a9c40dc7a9f71ef5e6a
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-10-02 22:48:03 +0200

    Install Material UI, add theme configuration, add Roboto font from Google fonts

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createTheme, ThemeProvider, CssBaseline } from "@mui/material";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
+const theme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StrictMode>
+);
