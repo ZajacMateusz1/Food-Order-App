e9f6220e142c1441aa7d52c3d6cfe6b6a5bf0bd0
Mateusz Zajac <144244766+ZajacMateusz1@users.noreply.github.com>
2025-10-07 00:27:24 +0200

    Add useFetch cunstom hook

---
+++ src/hooks/useFetch.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+export default function useFetch<T>(fetchFn: () => Promise<T>, startValue: T) {
+  const [data, setData] = useState<T>(startValue);
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  useEffect(() => {
+    async function fetchMeals() {
+      setIsLoading(true);
+      try {
+        const fetchedData = await fetchFn();
+        setData(fetchedData);
+      } catch (error) {
+        setError(
+          error instanceof Error
+            ? error.message
+            : "Can't fetch data! Try again later."
+        );
+      }
+      setIsLoading(false);
+    }
+    fetchMeals();
+  }, [fetchFn]);
+  return { data, error, isLoading };
+}
